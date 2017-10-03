8c91a053e82e66868a643cfaa2780c0be90edc7f
Michael Miller <michaelalanmiller@gmail.com>
2017-10-03 15:27:06 -0400

    Updated download

--- download_tweets.py
+++ download_tweets.py
@@ -1,30 +1,20 @@
-
-# coding: utf-8
-
-# In[1]:
-
 import os
 from subprocess import call
+import tqdm
 
-
-# # TREC2011
-
-# In[2]:
-
-trec_dir = '/home/michael/school/11-747/trec2011/'
-
-
-# In[16]:
+# Download tweets from the tweet ids provided by TREC2011
+trec_dir = 'trec2011/'
+app_path = 'twitter-tools/twitter-tools-core/target/appassembler/bin/AsyncHTMLStatusBlockCrawler'
 
 date_dirs = sorted([name for name in os.listdir(trec_dir) if os.path.isdir(os.path.join(trec_dir, name))])
 
-for date_dir in date_dirs[:1]:
+for date_dir in tqdm(date_dirs, desc="directories"):
     print(date_dir)
     date_dirpath = os.path.join(trec_dir, date_dir)
     
     dat_files = sorted([fname for fname in os.listdir(date_dirpath) if fname.endswith('.dat')])
-    for dat_fname in dat_files[1:2]:
-        app_path = '/home/michael/software/twitter-tools/twitter-tools-core/target/appassembler/bin/AsyncHTMLStatusBlockCrawler'
+
+    for dat_fname in tqdm(dat_files, desc="files"):
         out_path = os.path.join(trec_dir, date_dirpath, 'json', dat_fname[:-3] + 'json.gz')
         dat_path = os.path.join(trec_dir, date_dir, dat_fname)
         print("Processing {}...".format(dat_path))
