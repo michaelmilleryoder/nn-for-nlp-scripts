a453e6a2ad6951baf8931cf966e138446de4d061
Michael Alan Miller <mamille2@erebor.lti.cs.cmu.edu>
2017-10-03 16:11:07 -0400

    Updated download_tweets for server

--- download_tweets.py
+++ download_tweets.py
@@ -1,25 +1,33 @@
 import os
-from subprocess import call
-import tqdm
+import subprocess
+from tqdm import tqdm
 
 # Download tweets from the tweet ids provided by TREC2011
-trec_dir = 'trec2011/'
-app_path = 'twitter-tools/twitter-tools-core/target/appassembler/bin/AsyncHTMLStatusBlockCrawler'
+trec_dir = 'twitter/data/trec2011/'
+app_path = 'twitter/twitter-tools/twitter-tools-core/target/appassembler/bin/AsyncHTMLStatusBlockCrawler'
 
 date_dirs = sorted([name for name in os.listdir(trec_dir) if os.path.isdir(os.path.join(trec_dir, name))])
 
-for date_dir in tqdm(date_dirs, desc="directories"):
-    print(date_dir)
+#for date_dir in tqdm(date_dirs, desc="directories"):
+for date_dir in date_dirs: # are 17
+    tqdm.write(date_dir)
     date_dirpath = os.path.join(trec_dir, date_dir)
     
     dat_files = sorted([fname for fname in os.listdir(date_dirpath) if fname.endswith('.dat')])
 
-    for dat_fname in tqdm(dat_files, desc="files"):
-        out_path = os.path.join(trec_dir, date_dirpath, 'json', dat_fname[:-3] + 'json.gz')
-        dat_path = os.path.join(trec_dir, date_dir, dat_fname)
-        print("Processing {}...".format(dat_path))
-        call([app_path, '-data', dat_path, '-output', out_path])
-        print("\nOutput written to {}".format(out_path))
+    if not os.path.exists(os.path.join(date_dirpath, 'json')):
+        os.makedirs(os.path.join(date_dirpath, 'json'))
+
+    #for dat_fname in tqdm(dat_files, desc="files"):
+    for dat_fname in tqdm(dat_files):
+        out_path = os.path.join(date_dirpath, 'json', dat_fname[:-3] + 'json.gz')
+        dat_path = os.path.join(date_dirpath, dat_fname)
+        #tqdm.write("Processing {}...".format(dat_path))
+        subprocess.call([app_path, '-data', dat_path, '-output', out_path], stdout=subprocess.DEVNULL)
+        #subprocess.call([app_path, '-data', dat_path, '-output', out_path])
+            # piping output to suppress
+
+        #tqdm.write("\tOutput written to {}".format(out_path))
     
-    print()
+    #tqdm.write()
 
